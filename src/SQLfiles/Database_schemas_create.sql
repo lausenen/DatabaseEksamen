41d4799420eaee0308ea420dba7066a090bf99c0
Mia <miadong179@gmail.com>
2021-04-26 16:04:09 +0200

    få insertions

--- src/SQLfiles/Database_schemas_create.sql
+++ src/SQLfiles/Database_schemas_create.sql
@@ -133,3 +133,82 @@ CREATE TABLE Vagtplan (
   ON UPDATE CASCADE);
     
   show tables;
+
+INSERT Medarbejder VALUES
+(2102983231,'Sygeplejerske',32500, 'Jesper', 'Hjalmer', 'Yonassen',20210101),
+(1702832222,'Sygeplejerske',30000, 'Sybille','Bispebjerg', 'Pleyer',20210101),
+(2611672111,'Rengøringsassistent',18000, 'Gunnar', null, 'Frederiksen',20210101),
+(0101986748,'Lægestuderende', 15000, 'Søs', null, 'Tjerne',20210101),
+(1212996743,'Lægestuderende',15000, 'Pontus',null, 'Andersson',20210101);
+
+INSERT Vaccine VALUES
+('Covaxx',50),
+('Divoc', 25),
+('Blast3000',3000),
+('Aspera',100);
+
+INSERT Borger VALUES
+(1234567891,'Lars', null, 'Larsen','Blast3000'),
+(2611672101,'Margrethe', null, 'Milliard','Blast3000'),
+(0110987444,'Sys', null, 'Bjerre','Aspera'),
+(1234561111,'Anders',null, 'And','Divoc'),
+(3001025555,'Preben', null, 'Larsen','Blast3000'),
+(2611672111,'Jonne', null, 'Hansen','Blast3000'),
+(01019867470,'Ling', null, 'Ling','Aspera'),
+(1212996733,'Harald',null, 'Nyborg','Divoc'),
+(1706021234, 'Pedro', 'Josef', 'Santiago','Blast3000');
+
+INSERT Lokation VALUES
+('Kbh','Jagtvej', 17),
+('Hill','Paradisæblevej', 101),
+('Aarhus','Strøget', 10),
+('Kolding','Tvedvej', 57),
+('Odense','Windelsvej', 13),
+('Nakskov','Æbletoften', 7);
+
+/* virker af en mærkelig grund ikke
+INSERT Vaccine_Beholdning VALUES
+('Covaxx','Kbh',10000),
+('Covaxx','Hill',5000),
+('Covaxx','Aarhus',10000),
+('Covaxx','Kolding',5000),
+('Covaxx','Odsense',10000),
+('Covaxx','Naksskov',200),
+('Divoc','Kbh',2000),
+('Divoc','Hill',1000),
+('Divoc','Aarhus',3000),
+('Divoc','Kolding',1000),
+('Divoc','Odsense',3000),
+('Divoc','Naksskov',400),
+('Blast3000','Kbh',200),
+('Blast3000','Hill',100),
+('Blast3000','Aarhus',200),
+('Blast3000','Kolding',50),
+('Blast3000','Odsense',100),
+('Blast3000','Naksskov',20),
+('Aspera','Kbh',1000),
+('Aspera','Hill',500),
+('Aspera','Aarhus',1000),
+('Aspera','Kolding',500),
+('Aspera','Odsense',1000),
+('Aspera','Naksskov',200);
+
+SELECT* FROM Vaccine_Beholdning;
+*/
+
+
+
+-- SELECT* FROM Borger;
+SELECT* FROM Aftale;
+SELECT* FROM Ansat;
+-- SELECT* FROM Borger;
+SELECT* FROM Certificering;
+SELECT* FROM Vaccine_beholdning;
+SELECT* FROM Vagtplan;
+SELECT* FROM Vagt;
+-- SELECT* FROM Medarbejder;
+
+SELECT* FROM Lokation;
+SELECT* FROM Tidspunkt;
+SELECT* FROM Vaccine;
+
