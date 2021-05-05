bd2fca2512cc0eb1db227ac490e7b089b10ab1c7
Mia <miadong179@gmail.com>
2021-05-05 19:34:27 +0200

    rettet navne

--- src/SQLfiles/5Database_Instans.sql
+++ src/SQLfiles/5Database_Instans.sql
@@ -96,4 +96,17 @@ CREATE TABLE Vaccine_beholdning (
   ON DELETE CASCADE
   ON UPDATE CASCADE);
 
+CREATE VIEW Fuldtidsmedarbejdere AS
+SELECT Medarbejder_ID, Fornavn,Efternavn,Løn
+FROM Medarbejder
+WHERE Titel = 'Fuldtid'
+GROUP BY Løn DESC;
+
+CREATE VIEW Samlede_Vaccinationer AS
+SELECT DISTINCT Vaccine_Type, Count(Borger_ID) AS TOTAL
+FROM Aftale NATURAL JOIN Borger
+GROUP BY Vaccine_Type;
+
+
+
   show tables;
