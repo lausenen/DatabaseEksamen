1d4cec889dc7cc480b2e43cfb03817761bae5554
Mia <miadong179@gmail.com>
2021-05-06 13:39:18 +0200

    7.2.1-7.3

--- src/SQLfiles/7Forespørgsler.sql
+++ src/SQLfiles/7Forespørgsler.sql
@@ -25,17 +25,28 @@ SELECT Vaccine_Type,COUNT(Vaccination_Foretaget) AS Antal, (COUNT(Vaccination_Fo
 FROM Aftale_Pris_View WHERE Vaccination_Foretaget = true
 GROUP BY Vaccine_Type
 HAVING Total_Pris > 0;
+/*
+SELECT* FROM Aftale;
+*/
 
  -- 7.2.1 nr. 2
 SELECT Vaccine_Type, COUNT(Vaccination_Foretaget) * Pris AS Total_Pris
 FROM Aftale_Pris_View WHERE Vaccination_Foretaget = true and MONTH(Tidspunkt) = MONTH(current_date())
 GROUP BY Vaccine_Type
 HAVING Total_Pris > 0;
+/*
+-- Borgeren med ID = 1 skulle gerne have aftale med tidspunkt denne måned
+SELECT* FROM Aftale;
+*/
+
 
 /* Total afregning */
 -- 7.2.2
 
 SELECT SUM(Antal) AS TOTAL_ANTAL, SUM(Total_Pris) AS TOTAL_PRIS FROM Vaccine_Budget;
+/*
+UPDATE Aftale SET Vaccination_Foretaget = true WHERE Borger_ID = 1212996733 AND Tidspunkt = 20210527103000;
+*/
 
 /* Undersøge certificeret personale på given lokation */
 -- 7.3
@@ -44,6 +55,9 @@ WHERE Certificering.Medarbejder_ID = Ansat.Medarbejder_ID)
 AS Antal_Medarbejdere
 FROM Ansat NATURAL JOIN Certificering 
 GROUP BY Bynavn, Vaccine_Type;
+/*
+SELECT* FROM Certificering;
+*/
 
 /* Vaccinebehov pr. lokation */
  -- 7.4
